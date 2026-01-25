8e5714aac02fa381470e685787267abdaa8b5934
Stephen Booth <me@sbooth.org>
2026-01-25 12:04:02 -0600

    Update cSettings to cxxSettings (#7)

--- Package.swift
+++ Package.swift
@@ -9,46 +9,43 @@
 import PackageDescription
 
 let package = Package(
-	name: "CXXAudioHardware",
-	platforms: [
-		.macOS(.v11),
-	],
-	products: [
-		// Products define the executables and libraries a package produces, making them visible to other packages.
-		.library(
-			name: "CXXAudioHardware",
-			targets: [
-				"CXXAudioHardware",
-			]
-		),
-	],
-	dependencies: [
-		.package(url: "https://github.com/sbooth/CXXCoreAudio", from: "0.1.0")
-	],
-	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
-		.target(
-			name: "CXXAudioHardware",
-			dependencies: [
-				"CXXCoreAudio",
-			],
-			cSettings: [
-				.headerSearchPath("include/CXXAudioHardware"),
-			],
-			linkerSettings: [
-				.linkedFramework("CoreAudio"),
-			]
-		),
-		.testTarget(
-			name: "CXXAudioHardwareTests",
-			dependencies: [
-				"CXXAudioHardware",
-			],
-			swiftSettings: [
-				.interoperabilityMode(.Cxx),
-			]
-		),
-	],
-	cxxLanguageStandard: .cxx17
+    name: "CXXAudioHardware",
+    platforms: [
+        .macOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "CXXAudioHardware",
+            targets: [
+                "CXXAudioHardware",
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/sbooth/CXXCoreAudio", from: "0.1.0")
+    ],
+    targets: [
+        .target(
+            name: "CXXAudioHardware",
+            dependencies: [
+                "CXXCoreAudio",
+            ],
+            cxxSettings: [
+                .headerSearchPath("include/CXXAudioHardware"),
+            ],
+            linkerSettings: [
+                .linkedFramework("CoreAudio"),
+            ]
+        ),
+        .testTarget(
+            name: "CXXAudioHardwareTests",
+            dependencies: [
+                "CXXAudioHardware",
+            ],
+            swiftSettings: [
+                .interoperabilityMode(.Cxx),
+            ]
+        ),
+    ],
+    cxxLanguageStandard: .cxx17
 )
--- Tests/CXXAudioHardwareTests/CXXAudioHardwareTests.swift
+++ Tests/CXXAudioHardwareTests/CXXAudioHardwareTests.swift
@@ -9,8 +9,8 @@ import Testing
 @testable import CXXAudioHardware
 
 @Suite struct CXXAudioHardwareTests {
-	@Test func audioObject() async {
-		let obj = CXXAudioHardware.CAAudioObject()
-		#expect(obj.ObjectID() == kAudioObjectUnknown)
-	}
+    @Test func audioObject() async {
+        let obj = CXXAudioHardware.CAAudioObject()
+        #expect(obj.ObjectID() == kAudioObjectUnknown)
+    }
 }
